58f9e0514249a14e71b26b73db2ed2de2e5b5509
iklimov <respondatanytime@gmail.com>
2024-01-28 21:02:30 +0300

    feat: complete tasks in streams folder

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import fs from 'fs'
+import path from 'path'
+
+const __dirname = path.resolve()
+
+const filePath = path.join(__dirname, 'src', 'streams', 'files', 'fileToRead.txt')
+
 const read = async () => {
-    // Write your code here 
+    const readableStream = fs.createReadStream(filePath)
+
+    readableStream.on('error', err => {
+        throw new Error(err)
+    })
+
+    readableStream.on('data', chunck => {
+        process.stdout.write(chunck)
+    })
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,33 @@
+import fs from 'fs'
+import path from 'path'
+import readline from 'readline'
+
+const __dirname = path.resolve()
+
+const filePath = path.join(__dirname, 'src', 'streams', 'files', 'fileToWrite.txt')
+
 const write = async () => {
-    // Write your code here 
+    const writableStream = fs.createWriteStream(filePath)
+
+    writableStream.on('error', err => {
+        throw new Error(err)
+    })
+
+    const readlineInterface = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: 'Enter text: '
+    })
+
+    readlineInterface.prompt()
+
+    readlineInterface.on('line', line => {
+        writableStream.write(line)
+        readlineInterface.close()
+    }).on('close', () => {
+        writableStream.end()
+        process.exit()
+    })
 };
 
 await write();
