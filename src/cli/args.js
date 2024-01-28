8ac110cb01ff647f41c16eeb4d6cc75959eb7f1f
iklimov <respondatanytime@gmail.com>
2024-01-28 20:03:51 +0300

    feat: complete tasks in cli folder

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
+const executionPathArgumentIndex = 0
+const filePathArgumentIndex = 1
+
 const parseArgs = () => {
-    // Write your code here 
+    process.argv.forEach((_, argIndex) => {
+        const argName = process.argv[argIndex]
+        const argValue = process.argv[argIndex + 1]
+
+        if (argIndex === executionPathArgumentIndex || argIndex === filePathArgumentIndex) {
+            return
+        }
+
+        if (argIndex % 2 !== 0) {
+            return
+        }
+
+        process.stdout.write(`${argName.slice(2, argName.length)} is ${argValue}, `)
+    })
 };
 
 parseArgs();
